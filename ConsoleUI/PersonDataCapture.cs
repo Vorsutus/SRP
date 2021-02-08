28a7069f2e0c27cdda0c54133985a145b4242f24
CMRoss <ChrisRoss@u.boisestate.edu>
2021-02-07 21:51:23 -0700

    Add project files.

---
+++ ConsoleUI/PersonDataCapture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class PersonDataCapture
+    {
+        public static Person Capture()
+        {
+            // Ask for user information
+            Person output = new Person();
+
+            Console.Write("What is your first name: ");
+            output.FirstName = Console.ReadLine();
+
+            Console.Write("What is your last name: ");
+            output.LastName = Console.ReadLine();
+
+            Console.Write("What is your favorite number: ");
+            output.Number = Console.ReadLine();
+
+            return output;
+        }
+    }
+}
